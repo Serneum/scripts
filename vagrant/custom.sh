4295684076d45ecf45702f745214ccc2079901c8
Chris Rees <serneum@gmail.com>
2016-06-16 13:34:33 -0400

    Update script to rely on dotbot for configuration

--- vagrant/custom.sh
+++ vagrant/custom.sh
@@ -4,33 +4,16 @@ SETUP_FILE=/home/vagrant/setup.sh
 # the vagrant user
 cat <<EOF > $SETUP_FILE
 # Clone settings repo
-git clone https://github.com/Serneum/settings.git
-
-# Copy over Git settings
-cd ~/settings/git
-mv .gitconfig ~/.gitconfig
-mv .gitignore ~/.gitignore
-
-# Copy over bash settings
-cd ../home
-mv .bashrc ~/.bashrc
-if [ ! -d ~/.bash ]; then
-  mv .bash ~/
-fi
+git clone https://github.com/Serneum/dotfiles.git .dotfiles
+./.dotfiles/install
 
 # Set up Vim, Vundle, and install plugins from the .vimrc file
 if [ ! -d ~/.vim ]; then
   mkdir ~/.vim
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  mv .vimrc ~/.vimrc
-  
-  # Prevent the issue where Vim complains about input/output and causes the terminal to need to be closed
-  echo | echo | vim +PluginInstall +qall &>/dev/null
+  vundle
 fi
-
-# Remove the cloned repo
-cd ..
-rm -rf ~/settings
+brew install ag autojump cheat
 EOF
 
 # Give permissions for the script to the vagrant user
